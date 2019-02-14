403854e3e36d461f7e3f1dc40d6f857bb26f8f99
zhaolinxu <zhaolinxu@sina.cn>
2019-02-14 11:19:55 +0800

    更新

--- js/ui/GeneticsUi.js
+++ js/ui/GeneticsUi.js
@@ -2,13 +2,13 @@ define(["text!template/geneticsUi.html","action/Ascend","action/Mutate"],functio
 return a.prototype.display=function(t){this.container=t
 var a=[]
 for(var s in this.game.getMeta().mutations){var o=this.game.getMeta().mutations[s],r=this.game.getMutations()[o.id],m=null,u=""
-"fixed"==o.type?m=1+r.getDisplayBonus():(m=100*r.getDisplayBonus(),u="%"),a.push({id:o.id,name:o.name,description:o.description,bonus:Math.round(1e4*m)/1e4+u,genes:r.getGenes()})}this.container.html(i({mutations:a})),this.tickYear(),this.container.find("#ascendButton").click(function(){if(confirm("Are you sure you want to ascend?")){var t=new e(c.main)
+"fixed"==o.type?m=1+r.getDisplayBonus():(m=100*r.getDisplayBonus(),u="%"),a.push({id:o.id,name:o.name,description:o.description,bonus:Math.round(1e4*m)/1e4+u,genes:r.getGenes()})}this.container.html(i({mutations:a})),this.tickYear(),this.container.find("#ascendButton").click(function(){if(confirm("你确定要进行升天吗?")){var t=new e(c.main)
 t.ascend()}})
 var c=this
 this.container.find(".mutateButton").click(function(){var t={}
 c.container.find("input[name='genes']").each(function(){t[$(this).attr("data-id")]=$(this).val()})
 var e=new n(c.game)
-if(!e.canMutate(t))return void alert("You don't have that many genes!")
+if(!e.canMutate(t))return void alert("你没有那么多基因!")
 e.mutate(t)
 var i=c.container
 c.destroy(),c.display(i)})},a.prototype.tickYear=function(){var t=new BigNumber(0)
